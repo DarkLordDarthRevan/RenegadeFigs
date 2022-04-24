356acf1497eb90159a1333bf2655fcf73a099d67
dshemeluk <Dshemeluk@rrc.ca>
2022-04-24 11:30:38 -0500

    seventeen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_203310) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_205917) do
   create_table "abouts", force: :cascade do |t|
     t.string "title"
     t.text "body"
